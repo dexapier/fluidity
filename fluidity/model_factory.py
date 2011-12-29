b093e2e1685f07438cb974a60176f9a28bf1c59c
Jens Knutson <jens.knutson@gmail.com>
2011-12-28 22:20:12 -0600

    - Doesn't make sense to bother with a class for this anymore - just push all of the 'convert' code up into the top-level function

--- fluidity/model_factory.py
+++ fluidity/model_factory.py
@@ -36,7 +36,34 @@ _ENERGY_FROM_PROTO_VALUE = utils.invert_dict(_ENERGY_TO_PROTO_VALUE)
 
 def next_action_to_protobuf(na):
     """Convert a gee_tee_dee.NextAction to Protobuf-encoded bytes"""
-    return _NextActionToProtoConverter(na).convert()
+    proto = models.NextAction()
+    proto.metadata.uuid.raw_bytes = uuid.UUID(na.uuid).bytes
+    proto.metadata.creation_time.timestamp = utils.to_timestamp(na.creation_date)
+    proto.summary = na.summary
+    proto.priority = _PRIORITY_TO_PROTO_VALUE[na.priority]
+    
+    if na.completion_date:
+        proto.completion_time.timestamp = utils.to_timestamp(
+                na.completion_date)
+    
+    if na.queue_date:
+        proto.queue_time.timestamp = utils.to_timestamp(na.queue_date)
+    
+    if na.due_date:
+        proto.due_time.timestamp = utils.to_timestamp(na.due_date)
+    
+    proto.HACK_context = na.context
+    
+    proto.time_estimate_minutes = int(na.time_est)
+    proto.energy_estimate = _ENERGY_TO_PROTO_VALUE[na.energy_est]
+    
+    if na.notes:
+        proto.notes = unicode(na.notes)
+    
+    if na.url:
+        proto.related_resources.add().uri = na.url
+    
+    return proto
 
 
 # NOT DONE:
@@ -46,40 +73,3 @@ def next_action_to_protobuf(na):
 #    proto.status = _PROJECT_STATUS_TO_PROTO_VALUE[prj.status]
 #    if proto.status == models.Project.WAITING_FOR:
 #        proto.waiting_for_data = 
-    
-
-class _NextActionToProtoConverter(object):
-    
-    def __init__(self, next_action):
-        self._na = next_action
-
-    def convert(self):
-        proto = models.NextAction()
-        proto.metadata.uuid.raw_bytes = uuid.UUID(self._na.uuid).bytes
-        proto.metadata.creation_time.timestamp = utils.to_timestamp(
-                self._na.creation_date)
-        proto.summary = self._na.summary
-        proto.priority = _PRIORITY_TO_PROTO_VALUE[self._na.priority]
-
-        if self._na.completion_date:
-            proto.completion_time.timestamp = utils.to_timestamp(
-                    self._na.completion_date)
-
-        if self._na.queue_date:
-            proto.queue_time.timestamp = utils.to_timestamp(self._na.queue_date)
-        
-        if self._na.due_date:
-            proto.due_time.timestamp = utils.to_timestamp(self._na.due_date)
-        
-        proto.HACK_context = self._na.context
-        
-        proto.time_estimate_minutes = int(self._na.time_est)
-        proto.energy_estimate = _ENERGY_TO_PROTO_VALUE[self._na.energy_est]
-        
-        if self._na.notes:
-            proto.notes = unicode(self._na.notes)
-        
-        if self._na.url:
-            proto.related_resources.add().uri = self._na.url
-
-        return proto

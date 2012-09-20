dd906d2155162b8e4bee9f66eb7c331ec384415e
Jens Knutson <jens.knutson@gmail.com>
2012-09-20 09:47:06 -0500

    Remove tools subpackage from setup.py; doesn't apply anymore

--- setup.py
+++ setup.py
@@ -81,7 +81,7 @@ setup(
                    'Topic :: Desktop Environment :: File Managers',
                    'Topic :: Desktop Environment :: Gnome',
                    'Topic :: Office/Business',],
-    packages = ['fluidity', 'fluidity.ui', 'fluidity.tools'],
+    packages = ['fluidity', 'fluidity.ui'],
     scripts = ["bin/fluidity", "bin/slider"],
     provides = ["fluidity"],
     data_files = build_data_files()
